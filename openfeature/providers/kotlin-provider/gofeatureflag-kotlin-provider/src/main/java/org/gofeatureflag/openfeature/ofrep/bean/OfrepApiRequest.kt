e043ea3c19d53e315c2440c8f75bddfc544377ba
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2025-10-10 16:03:57 +0200

    feat: WIP
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- openfeature/providers/kotlin-provider/gofeatureflag-kotlin-provider/src/main/java/org/gofeatureflag/openfeature/ofrep/bean/OfrepApiRequest.kt
+++ openfeature/providers/kotlin-provider/gofeatureflag-kotlin-provider/src/main/java/org/gofeatureflag/openfeature/ofrep/bean/OfrepApiRequest.kt
@@ -2,4 +2,5 @@ import dev.openfeature.sdk.EvaluationContext
 
 data class OfrepApiRequest(@Transient val ctx: EvaluationContext) {
     private val context: Map<String, Any?> = ctx.asObjectMap().plus("targetingKey" to ctx.getTargetingKey())
+
 }
